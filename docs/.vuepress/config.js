411bba6cc6d82e513f16499ea71c7603c605ac6a
IGunKe <xdjyyds@yeah.net>
2022-11-05 21:12:33 +0800

    2022-11-5

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -100,7 +100,8 @@ module.exports = {
 				path: '/pages/Es5/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
 				collapsable: false, // 可选的, 默认值是 true,
 				sidebarDepth: 1, // 可选的, 默认值是 1
-				children: ['/pages/Es5/01.md',
+				children: [
+					'/pages/Es5/01.md',
 					'/pages/Es5/02.md',
 					'/pages/Es5/03.md',
 					'/pages/Es5/04.md',
